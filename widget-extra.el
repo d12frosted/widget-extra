6fdf9c01aa1e78ef1206b57d26666f92a42e2b5a
Boris Buliga <boris@d12frosted.io>
2026-01-06 09:38:16 +0200

    chore: update copyright years to 2026

--- widget-extra.el
+++ widget-extra.el
@@ -1,6 +1,6 @@
 ;;; widget-extra.el --- Extra widgets that extend built-in library  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024-2025  Boris Buliga
+;; Copyright (C) 2024-2026  Boris Buliga
 
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
